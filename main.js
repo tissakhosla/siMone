224448a7b8195de15c614a6ff265f7167e7212aa
Tissa Khosla <tissa.music@gmail.com>
2019-10-22 01:03:43 -0400

    experimenting with these for loops

--- main.js
+++ main.js
@@ -90,44 +90,53 @@
 
 
 
-let game = [0,1,2,3,2,3,1,4]
-let card = document.querySelectorAll(".card")
+const game = [0,1,2,3,2,3,1,4]
+const card = document.querySelectorAll(".card")
+let time = 500
+let timeMult = 1
 
+setTimeout(function(){card[game[0]].style.backgroundColor = "red"}, time*timeMult)
+timeMult++
+setTimeout(function(){card[game[0]].style.backgroundColor = ""}, time*timeMult)
+setTimeout(function(){card[game[1]].style.backgroundColor = "red"}, time*timeMult)
+timeMult++
+setTimeout(function(){card[game[1]].style.backgroundColor = ""}, time*timeMult)
+setTimeout(function(){card[game[2]].style.backgroundColor = "red"}, time*timeMult)
+timeMult++
+setTimeout(function(){card[game[2]].style.backgroundColor = ""}, time*timeMult)
+setTimeout(function(){card[game[3]].style.backgroundColor = "red"}, time*timeMult)
+timeMult++
+setTimeout(function(){card[game[3]].style.backgroundColor = ""}, time*timeMult)
+setTimeout(function(){card[game[4]].style.backgroundColor = "red"}, time*timeMult)
+timeMult++
+setTimeout(function(){card[game[4]].style.backgroundColor = ""}, time*timeMult)
+setTimeout(function(){card[game[5]].style.backgroundColor = "red"}, time*timeMult)
+timeMult++
+setTimeout(function(){card[game[5]].style.backgroundColor = ""}, time*timeMult)
+setTimeout(function(){card[game[6]].style.backgroundColor = "red"}, time*timeMult)
+timeMult++
+setTimeout(function(){card[game[6]].style.backgroundColor = ""}, time*timeMult)
+setTimeout(function(){card[game[7]].style.backgroundColor = "red"}, time*timeMult)
+timeMult++
 
-// setTimeout(function(){card[game[0]].style.backgroundColor = "red"}, 1000)
-
-// setTimeout(function(){card[game[0]].style.backgroundColor = ""}, 2000)
-// setTimeout(function(){card[game[1]].style.backgroundColor = "red"}, 2000)
-
-// setTimeout(function(){card[game[1]].style.backgroundColor = ""}, 3000)
-// setTimeout(function(){card[game[2]].style.backgroundColor = "red"}, 3000)
-
-// setTimeout(function(){card[game[2]].style.backgroundColor = ""}, 4000)
-// setTimeout(function(){card[game[3]].style.backgroundColor = "red"}, 4000)
-
-// setTimeout(function(){card[game[3]].style.backgroundColor = ""}, 5000)
-// setTimeout(function(){card[game[4]].style.backgroundColor = "red"}, 5000)
-
-// setTimeout(function(){card[game[4]].style.backgroundColor = ""}, 5000)
-// setTimeout(function(){card[game[5]].style.backgroundColor = "red"}, 5000)
-
-// setTimeout(function(){card[game[5]].style.backgroundColor = ""}, 6000)
-// setTimeout(function(){card[game[6]].style.backgroundColor = "red"}, 6000)
-
-// setTimeout(function(){card[game[6]].style.backgroundColor = ""}, 7000)
-// setTimeout(function(){card[game[7]].style.backgroundColor = "red"}, 7000)
+setTimeout(function(){
+   for (var i = 0; i < card.length; i++){
+      card[i].style.backgroundColor = "blue"
+      console.log(card[i])
+   }
+}, time*timeMult)
 
 setInterval(function(){
    for (var i = 0; i < game.length; i++){
       card[game[i]].style.backgroundColor = "red"
    }
-}, 200)
+}, time*timeMult/2)
 
 setInterval(function(){
    for (var i = 0; i < card.length; i++){
       card[i].style.backgroundColor = "blue"
    }
-}, 400)
+}, time*timeMult*2)
 
     
 

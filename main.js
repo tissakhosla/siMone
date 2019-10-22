4dc13510e021497003137679e24a9c3d1ca8a25b
Tissa Khosla <tissa.music@gmail.com>
2019-10-21 21:21:53 -0400

    failed loop. Dig it

--- main.js
+++ main.js
@@ -3,30 +3,39 @@ let game = [0,2,1,3,2,4,0]
 let buttons = document.querySelectorAll(".button")
 console.dir(buttons)
 
+let time = 0
+for(let i = 0; i < game.length; i++){
+    setTimeout(function(){
+        buttons[game[i]].style.backgroundColor = "red"
+        time += 500
+        setTimeout(function(){buttons[game[i]].style.backgroundColor = ""}, time)
+    }, time)
+}
+
 
 
     
-setTimeout(function(){buttons[game[0]].style.backgroundColor = "red"}, 1000)
+// setTimeout(function(){buttons[game[0]].style.backgroundColor = "red"}, 1000)
 
-setTimeout(function(){buttons[game[0]].style.backgroundColor = ""}, 2000)
-setTimeout(function(){buttons[game[1]].style.backgroundColor = "red"}, 2000)
+// setTimeout(function(){buttons[game[0]].style.backgroundColor = ""}, 2000)
+// setTimeout(function(){buttons[game[1]].style.backgroundColor = "red"}, 2000)
 
-setTimeout(function(){buttons[game[1]].style.backgroundColor = ""}, 3000)
-setTimeout(function(){buttons[game[2]].style.backgroundColor = "red"}, 3000)
+// setTimeout(function(){buttons[game[1]].style.backgroundColor = ""}, 3000)
+// setTimeout(function(){buttons[game[2]].style.backgroundColor = "red"}, 3000)
 
-setTimeout(function(){buttons[game[2]].style.backgroundColor = ""}, 4000)
-setTimeout(function(){buttons[game[3]].style.backgroundColor = "red"}, 4000)
+// setTimeout(function(){buttons[game[2]].style.backgroundColor = ""}, 4000)
+// setTimeout(function(){buttons[game[3]].style.backgroundColor = "red"}, 4000)
 
-setTimeout(function(){buttons[game[3]].style.backgroundColor = ""}, 5000)
-setTimeout(function(){buttons[game[4]].style.backgroundColor = "red"}, 5000)
+// setTimeout(function(){buttons[game[3]].style.backgroundColor = ""}, 5000)
+// setTimeout(function(){buttons[game[4]].style.backgroundColor = "red"}, 5000)
 
-setTimeout(function(){buttons[game[4]].style.backgroundColor = ""}, 5000)
-setTimeout(function(){buttons[game[5]].style.backgroundColor = "red"}, 5000)
+// setTimeout(function(){buttons[game[4]].style.backgroundColor = ""}, 5000)
+// setTimeout(function(){buttons[game[5]].style.backgroundColor = "red"}, 5000)
 
-setTimeout(function(){buttons[game[5]].style.backgroundColor = ""}, 6000)
-setTimeout(function(){buttons[game[6]].style.backgroundColor = "red"}, 6000)
+// setTimeout(function(){buttons[game[5]].style.backgroundColor = ""}, 6000)
+// setTimeout(function(){buttons[game[6]].style.backgroundColor = "red"}, 6000)
 
-setTimeout(function(){buttons[game[6]].style.backgroundColor = ""}, 7000)
+// setTimeout(function(){buttons[game[6]].style.backgroundColor = ""}, 7000)
     
 
 console.log ("Wear Elive")

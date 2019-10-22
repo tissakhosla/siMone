00504dd2f176ab0afaad22140fee88a22de970c6
Tissa Khosla <tissa.music@gmail.com>
2019-10-22 18:35:04 -0400

    office hours

--- main5.js
+++ main5.js
@@ -2,22 +2,43 @@
 
 
 let game = [0,1,2,4,0,3,0]
-let card = document.querySelectorAll(".card")
+let cards = document.querySelectorAll(".card")
 let t = 500
 let form = document.querySelector(".keyboard")
-let steps = []
+let eachStep = []
 
-// game.forEach
-
-function flash(){
-   card[game[0]].style.backgroundColor = "red"
-   setTimeout(function unflash(){
-      card[game[0]].style.backgroundColor = ""
-   }, t)
-   setTimeout(flash, t + 500)
+for (var s = 1; s <= game.length; s++){
+   let steps = []
+   eachStep.push(steps.concat(game.slice(0,s)))
 }
+console.dir(eachStep)
 
-setTimeout(flash(game), t)
+
+// for (var i = 0; i < cards.length; i++){
+//    cards[i].addEventListener("click", function(){
+//       console.log("clicked")
+//    })
+// }
+
+// game.forEach(
+//    function (element){
+//          card[element].style.backgroundColor = "red"
+//          setTimeout(flash, t + 500)
+//    }
+// )   
+
+
+// function flash(){
+//    setTimeout(function() {
+//       card[game[0]].style.backgroundColor = "red"
+//       setTimeout(function unflash(){
+//          card[game[0]].style.backgroundColor = ""
+//       }, t)
+//       setTimeout(flash, t + 500)
+//    }, t)
+// }
+
+// flash()
 
 // for(var i = 0;i < game.length; i++){
 //    let k = i;
@@ -29,7 +50,13 @@ setTimeout(flash(game), t)
 //    }, t * (k + 2));
 // }
 
-// for (var s = 1; s < game.length; s++){
-//    let thisStep = steps
-//    console.log(steps.concat(game.slice(0,s)))
+
+// function add(sequence){
+//    let steps = []
+//    for (var s = 1; s <= sequence.length; s++){
+//       let thisStep = steps.concat(sequence.slice(0,s))
+//       console.log(thisStep)
+//       }
+//    }
 // }
+
